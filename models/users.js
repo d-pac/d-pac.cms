aeb1f821b9bfbb227e42a0f2448aa0f1cc2c7742
Camille Reynders <camille.reynders@gmail.com>
2014-05-26 14:24:05 +0200

    Reformat js code

--- models/users.js
+++ models/users.js
@@ -1,34 +1,32 @@
-var _ = require('underscore'),
-	keystone = require('keystone'),
-	Types = keystone.Field.Types;
+var _ = require( 'underscore' ),
+  keystone = require( 'keystone' ),
+  Types = keystone.Field.Types;
 
 /**
  * Users
  * =====
  */
 
-var User = new keystone.List('User');
+var User = new keystone.List( 'User' );
 
-User.add({
-	name: { type: Types.Name, required: true, index: true },
-	email: { type: Types.Email, initial: true, required: true, index: true },
-	password: { type: Types.Password, initial: true, required: false }
+User.add( {
+  name     : { type : Types.Name, required : true, index : true },
+  email    : { type : Types.Email, initial : true, required : true, index : true },
+  password : { type : Types.Password, initial : true, required : false }
 }, 'Permissions', {
-	isAdmin: { type: Boolean, label: 'Can access Keystone' }
-});
+  isAdmin : { type : Boolean, label : 'Can access Keystone' }
+} );
 
 // Provide access to Keystone
-User.schema.virtual('canAccessKeystone').get(function() {
-	return this.isAdmin;
-});
-
+User.schema.virtual( 'canAccessKeystone' ).get( function(){
+  return this.isAdmin;
+} );
 
 /**
  * Relationships
  */
 
-User.relationship({ ref: 'Post', path: 'author' });
-
+User.relationship( { ref : 'Post', path : 'author' } );
 
 /**
  * Registration
--- routes/views/index.js
+++ routes/views/index.js
@@ -1,14 +1,15 @@
-var keystone = require('keystone');
+var keystone = require( 'keystone' );
 
-exports = module.exports = function(req, res) {
-	
-	var locals = res.locals,
-		view = new keystone.View(req, res);
-	
-	// Set locals
-	locals.section = 'home';
-	
-	// Render the view
-	view.render('index');
-	
+exports = module.exports = function( req,
+                                     res ){
+
+  var locals = res.locals,
+    view = new keystone.View( req, res );
+
+  // Set locals
+  locals.section = 'home';
+
+  // Render the view
+  view.render( 'index' );
+
 }
