ae3518f4e5c55fe5356fe9475648a41dea21d23a
Camille Reynders <camille.reynders@gmail.com>
2014-06-23 10:11:07 +0200

    Update rest API to use keystone-rest v0.0.6

--- routes/rest.js
+++ routes/rest.js
@@ -3,15 +3,31 @@ var _ = require( 'underscore' );
 var keystone = require( 'keystone' );
 var keystoneRest = require( 'keystone-rest' );
 
+var excludedUserFields = ['password', 'isAdmin', 'email', '__v', '_r'];
+
 keystoneRest.exposeRoutes( keystone.list( 'User' ), {
-  get    : { omit : ['password'] },
-  put    : { omit : ['password'] }
+  get : { 
+    omit : excludedUserFields 
+  },
+  put : { 
+    omit : ['password'] 
+  }
+} );
+
+keystoneRest.exposeRoutes( keystone.list( 'Representation' ), {
+  get : {
+    omit          : ['__v', '_r'],
+    relationships : {
+      assessee : {
+        omit : excludedUserFields
+      }
+    }
+  }
 } );
 
 exports = module.exports = {
-  register : function register(app){
-    _.each( keystoneRest.routes, function( route ){
-      app[route.method]( route.route, route.handler );
-    } );    
+  register : function register( app ){
+    console.log( keystoneRest.routes );
+    keystoneRest.registerRoutes(app);
   }
 };
