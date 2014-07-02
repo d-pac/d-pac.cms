9f2ec54f89ea18aacc51c3aa7c6273383fb13752
Camille Reynders <camille.reynders@gmail.com>
2014-07-02 14:31:04 +0200

    Add release task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,16 @@
 var dotenv = require( 'dotenv' );
 dotenv.load();
 
+var semver = require('semver');
+
 module.exports = function( grunt ){
   // Time how long tasks take. Can help when optimizing build times
   require( 'time-grunt' )( grunt );
 
   // Load grunt tasks automatically
   require( 'jit-grunt' )( grunt, {
-
+    "bump-only"   : "grunt-bump",
+    "bump-commit" : "grunt-bump"
   } );
 
   var configs = require( 'load-grunt-configs' )( grunt, {
@@ -19,7 +22,7 @@ module.exports = function( grunt ){
     },
     env   : process.env
   } );
-  
+
   // Project configuration.
   grunt.initConfig( configs );
 
@@ -38,10 +41,19 @@ module.exports = function( grunt ){
     ] );
   } );
 
-  grunt.registerTask( 'server', function(){
-    grunt.log.warn( 'The `server` task has been deprecated. Use `grunt serve` to start a server.' );
-    grunt.task.run( ['serve:' + target] );
-  } );
+  grunt.registerTask('release', function(versionOrType){
+ 		var bumpTask = 'bump-only';
+ 		if(!versionOrType){
+ 			bumpTask += ':patch';
+ 		}else if(semver.clean(versionOrType)){
+ 			grunt.option('setversion', versionOrType);
+ 		}else{
+ 			bumpTask += ':' + versionOrType;
+ 		}
+ 		grunt.task.run([
+ 			'lint', bumpTask, 'bump-commit'
+ 		]);
+ 	});
 
   grunt.registerTask( 'deploy', ['rsync'] );
 
