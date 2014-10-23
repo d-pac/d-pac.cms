65c34cd032d5b1ca4e8ba5694d685bf8037909a7
Camille Reynders <camille.reynders@gmail.com>
2014-10-23 14:56:17 +0200

    Allow for keystone field type validation for Users

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,9 @@
 var debug = require( 'debug' )( 'dpac:api.users' );
 var keystone = require( 'keystone' );
 var _ = require( 'underscore' );
+var errors = require('errors');
 var Controller = require( './Controller' );
+var utils = require( './utils' );
 var service = require( '../../services/users' );
 var schema = keystone.list( 'User' );
 
@@ -22,12 +24,33 @@ var update = module.exports.update = function( req,
                                                res,
                                                next ){
   debug( '#update' );
-  controller.update( {
-    values : {
-      _id : req.user.id
-    },
-    fields : schema.api.editable
-  }, req, res, next );
+  var fields = schema.api.editable;
+  var values = utils.parseValues( {
+    //the `getUpdateHandler` needs only the names of the fields, really in the model
+    //BUT it does need a `password_confirm` member in the data object
+    fields : ['password_confirm'].concat(fields)
+  }, req );
+
+  service
+    .retrieve( { _id : req.user.id } )
+    .then( function( user ){
+      //we need to use this atrocity since otherwise the keystone-native validators won't run
+      user.getUpdateHandler( req, res ).process( values, {
+        fields      : fields,
+        flashErrors : false
+      }, function( err,
+                   processor ){
+        console.log('hmmm');
+        if( err ){
+          return next( err );
+        }
+        var user = processor.item;
+        if( !user ){
+          return next( new errors.Http500Error() );
+        }
+        return res.apiResponse( user );
+      } );
+    } );
 };
 
 module.exports.replace = function( req,
--- services/users.js
+++ services/users.js
@@ -20,27 +20,5 @@ module.exports.retrieve = function retrieve( opts ){
   return schema.model
     .findById( opts._id )
     .populate('organization')
-    .lean()
     .exec();
-};
-
-/**
- *
- * @param opts
- * @param {string} opts._id User.id
- * @param {function} validator
- */
-var update = module.exports.update = function update( opts ){
-  debug( 'update' );
-  return schema.model
-    .findById( opts._id )
-    .exec()
-    .then( function( doc ){
-      if( !doc ){
-        return;
-      }
-      extend( doc, opts );
-      var save = Promise.promisify(doc.save, doc);
-      return save();
-    } );
 };
