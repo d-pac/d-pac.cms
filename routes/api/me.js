b1f87293c3bf6a746ad6a44c475322247aa94b4e
Camille Reynders <camille.reynders@gmail.com>
2014-11-03 12:26:48 +0100

    Select assessment automatically when assessor has no comparison in progress
    Fix d-pac/d-pac.client#46

--- routes/api/me.js
+++ routes/api/me.js
@@ -6,31 +6,15 @@ var async = require( 'async' );
 var errors = require( 'errors' );
 var debug = require( 'debug' )( 'dpac:api.me' );
 var constants = require( '../../models/helpers/constants' );
-var Promise = require('bluebird');
+var Promise = require( 'bluebird' );
 
 var comparisons = require( '../../services/comparisons' );
 var personas = require( '../../services/personas' );
 var mementos = require( '../../services/mementos' );
 
-module.exports.listMementos = function( req,
-                                        res,
-                                        next ){
-  debug( '#listMementos' );
-  mementos.listActives( {
-    assessor : req.user.id
-  } ).onResolve( function( err,
-                           result ){
-    if( err ){
-      return next( err );
-    }
-
-    res.apiResponse( result );
-  } );
-};
-
-module.exports.createMemento = function( req,
-                                         res,
-                                         next ){
+var createMemento = module.exports.createMemento = function( req,
+                                                             res,
+                                                             next ){
   debug( '#create' );
   mementos.create( {
     assessor   : req.user.id,
@@ -45,38 +29,69 @@ module.exports.createMemento = function( req,
   } );
 };
 
+module.exports.listMementos = function( req,
+                                        res,
+                                        next ){
+  debug( '#listMementos' );
+  mementos.listActives( {
+    assessor : req.user.id
+  } ).onResolve( function( err,
+                           result ){
+    if( err ){
+      return next( err );
+    }
+
+    if( !result || result.length <= 0 ){
+      personas.listAssessments( {
+        user : req.user.id,
+        role : constants.roles.assessor
+      } ).then( function( assessments ){
+        if( assessments && assessments.length > 0 ){
+          mementos.create( {
+            assessor   : req.user.id,
+            assessment : assessments[0]._id
+          } ).onResolve( function( err,
+                                   result ){
+            if( err ){
+              return next( err );
+            }
+
+            res.apiResponse( [result] );
+          } );
+        }else{
+          res.apiResponse([]);
+        }
+      } );
+    }else{
+      //in progress
+      res.apiResponse( result );
+    }
+  } );
+};
+
 module.exports.listAssessments = function( req,
                                            res,
                                            next ){
   debug( '#listAssessments' );
 
   var output = [];
-  personas.list( {
+  personas.listAssessments( {
     user : req.user.id,
     role : constants.roles.assessor
-  } ).then( function handlePersonas( docs ){
-    return _.chain( docs )
-      .map( function( persona ){
-        return persona.assessment;
-      } )
-      .filter( function( assessment ){
-        return !!assessment && (assessment.state === constants.publicationStates.published);
-      } )
-      .value();
   } ).then( function( assessments ){
     var promises = [];
     _.each( assessments, function( assessment ){
       var p = comparisons.completedCount( {
-        assessor : req.user.id,
+        assessor   : req.user.id,
         assessment : assessment._id
       } ).then( function handleComparisonsNum( completedComparisons ){
-        if(completedComparisons < assessment.comparisonsNum){
-          output.push(assessment);
+        if( completedComparisons < assessment.comparisonsNum ){
+          output.push( assessment );
         }
       } );
-      promises.push(p);
+      promises.push( p );
     } );
-    return Promise.all(promises);
+    return Promise.all( promises );
   } ).onResolve( function( err,
                            assessments ){
     if( err ){
--- services/personas.js
+++ services/personas.js
@@ -1,25 +1,41 @@
 'use strict';
 var debug = require( 'debug' )( 'dpac:services.personas' );
-var _ = require('underscore');
+var _ = require( 'underscore' );
 var keystone = require( 'keystone' );
 var schema = keystone.list( 'Persona' );
+var constants = require( '../models/helpers/constants' );
 
-var listById = module.exports.listById = function listById(ids){
+var listById = module.exports.listById = function listById( ids ){
   return schema.model
     .find()
     .where( '_id' ).in( ids )
-    .populate('assessment')
+    .populate( 'assessment' )
     .exec();
 };
 
-module.exports.list = function list( opts ){
-  debug('list');
-  if( _.isArray(opts)){
-    return listById(opts);
+var list = module.exports.list = function list( opts ){
+  debug( 'list' );
+  if( _.isArray( opts ) ){
+    return listById( opts );
   }
 
   return schema.model
-    .find(opts)
-    .populate('assessment')
+    .find( opts )
+    .populate( 'assessment' )
     .exec();
 };
+
+module.exports.listAssessments = function listAssessments( opts ){
+  debug("#listAssessments");
+  return list( opts )
+    .then( function handlePersonas( docs ){
+      return _.chain( docs )
+        .map( function( persona ){
+          return persona.assessment;
+        } )
+        .filter( function( assessment ){
+          return !!assessment && (assessment.state === constants.publicationStates.published);
+        } )
+        .value();
+    } );
+}
