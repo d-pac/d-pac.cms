15cbb78c5c506f544e33a048c32fd0110178ad12
Camille Reynders <camille.reynders@gmail.com>
2014-11-05 09:38:24 +0100

    Add script for mixed assessment installation

---
+++ scripts/install-mixed-assessments.js
@@ -0,0 +1,8 @@
+'use strict';
+var path = require( 'path' );
+var pkg = require( '../package.json' );
+var fs = require( 'fs' );
+
+var src = path.resolve( path.join( __dirname, 'mixed-assessments.js' ) );
+var dest = path.resolve( path.join( __dirname, "../updates", pkg.version + "-mixed-assessments.js" ) );
+fs.createReadStream( src ).pipe( fs.createWriteStream( dest ) );
