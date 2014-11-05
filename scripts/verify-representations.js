252c9ecbcb376a8ffdf292e4ee1a43efb4ab986d
Camille Reynders <camille.reynders@gmail.com>
2014-11-05 10:39:57 +0100

    Add script for verifying all representations are valid

---
+++ scripts/verify-representations.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var konfy = require( 'konfy' );
+konfy.load();
+
+var expect = require( 'must' );
+
+var mongoose = require( 'mongoose' );
+
+mongoose.connect( process.env.MONGO_URI );
+
+var db = mongoose.connection;
+db.on( 'error', console.error.bind( console, 'connection error:' ) );
+db.once( 'open', function callback(){
+  var Representation = mongoose.model( 'representations', mongoose.Schema() );
+  Representation.find().lean().exec().then( function( representations ){
+
+    representations.forEach( function( representation ){
+      console.log( "Verifying:", representation._id );
+      try{
+        expect( representation ).to.have.property( 'file' );
+      }catch( err ){
+        console.log( '[Error]', 'Representation', representation._id, 'missing "file"' );
+      }
+    } );
+
+    mongoose.connection.close();
+  } );
+} );
