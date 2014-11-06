77df035b564e9d4223bf29bfc9874681e0441206
Camille Reynders <camille.reynders@gmail.com>
2014-11-06 12:37:42 +0100

    Add specific persona deletion

---
+++ scripts/delete-specific-personas.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var konfy = require( 'konfy' );
+konfy.load();
+
+var expect = require( 'must' );
+var _ = require( 'underscore' );
+
+var mongoose = require( 'mongoose' );
+var objectId = mongoose.Types.ObjectId;
+mongoose.connect( process.env.MONGO_URI );
+
+var userNames = [
+  "102", "103", "104", "106", "107", "108",
+  "201", "208", "210", "212", "213", "214",
+  "301", "307", "310", "314",
+  "401", "403", "408", "409", "410", "411", "412", "413", "414", "415", "416", "418", "419", "420", "421", "422", "424",
+  "425",
+  "501", "502", "505", "506", "507", "508", "509", "512", "513", "515", "516", "519", "520",
+  "602", "606", "607", "608", "609", "611", "614", "615", "616",
+  "701", "703", "704", "705", "707", "710",
+  "801", "802", "803", "804", "805", "808", "810", "811", "812", "813", "814", "815", "816", "817", "818", "819", "820",
+  "821",
+  "901", "902", "903", "904", "905", "906", "909", "910", "911", "912", "914", "915", "917", "918",
+  "1001", "1002", "1003", "1005", "1006", "1008", "1009"
+];
+var assessmentId = objectId( "545889960138e02976448d28" );
+
+var db = mongoose.connection;
+db.on( 'error', console.error.bind( console, 'connection error:' ) );
+db.once( 'open', function callback(){
+  var User = mongoose.model( 'users', mongoose.Schema() );
+  var Persona = mongoose.model( 'personas', mongoose.Schema() );
+  var Representation = mongoose.model( 'representations', mongoose.Schema() );
+  var userIds;
+  User.find()
+    .where( "name.first" )
+    .in( userNames )
+    .lean()
+    .exec()
+    .then( function( users ){
+      userIds = _.pluck( users, "_id" );
+      return userIds;
+    } )
+    .then( function(){
+      return Persona.find( {
+        assessment : assessmentId
+      } )
+        .where( "user" ).in( userIds )
+        .remove()
+        .exec();
+    } ).then( function( deletedNum ){
+      console.log( "Deleted", deletedNum, "personas" );
+    } )
+    .then( function(){
+      return Representation.find( {
+        assessment : assessmentId
+      } ).where( "assessee" ).in( userIds )
+        .remove()
+        .exec();
+    } )
+    .then( function( deletedNum ){
+      console.log( "Deleted", deletedNum, "representations" );
+    } )
+    .then( function(){
+      mongoose.connection.close();
+    } );
+} );
