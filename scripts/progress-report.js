f9f126e0e4c391c9c7c0d9d85260c1df32f0d3c8
Camille Reynders <camille.reynders@gmail.com>
2014-11-18 10:55:09 +0100

    First sketch of progress report

---
+++ scripts/progress-report.js
@@ -0,0 +1,37 @@
+'use strict';
+var keystone = require( 'keystone' );
+var _ = require( 'underscore' );
+var objectId = require( 'mongoose' ).Types.ObjectId;
+var fs = require( "fs" );
+
+var Comparison = keystone.list( 'Comparison' );
+
+function listComparisons(){
+  return Comparison.model
+    .aggregate()
+    .group( {
+      _id : "$assessor",
+      total: {$sum: 1}
+    } )
+    .project( {
+      assessor  : '$_id',
+      _id       : 0,
+      total : 1
+    } )
+    .sort( "-total" )
+    .exec( function( err,
+                     results ){
+      if( err ){
+        console.error( err );
+      }
+      console.log( results );
+
+    } );
+}
+
+exports = module.exports = function( done ){
+  listComparisons()
+    .then( function(){
+      done();
+    } );
+};
