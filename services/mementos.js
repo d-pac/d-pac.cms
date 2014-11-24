351d020d7dd4e93d29f175baec0280bc060110bf
Camille Reynders <camille.reynders@gmail.com>
2014-11-24 11:32:41 +0100

    Fix bug with flipped representations

--- services/mementos.js
+++ services/mementos.js
@@ -7,13 +7,12 @@ var errors = require( 'errors' );
 var Promise = require( 'bluebird' );
 var toSafeJSON = require( './utils' ).toSafeJSON;
 
-var representations = require( './representations' );
-var judgements = require( './judgements' );
-var comparisons = require( './comparisons' );
-var assessments = require( './assessments' );
-var phases = require( './phases' );
-var seqs = require( './seqs' );
-var timelogs = require( './timelogs' );
+var representationsService = require( './representations' );
+var judgementsService = require( './judgements' );
+var comparisonsService = require( './comparisons' );
+var assessmentsService = require( './assessments' );
+var phasesService = require( './phases' );
+var seqsService = require( './seqs' );
 
 /**
  *
@@ -28,7 +27,7 @@ module.exports.create = function createMemento( opts ){
   var memento = {
     assessor : opts.assessor
   };
-  return assessments.retrieve( { _id : opts.assessment } )
+  return assessmentsService.retrieve( { _id : opts.assessment } )
     .then( function handleAssessment( assessment ){
       if( !assessment ){
         throw new errors.Http422Error( {
@@ -40,8 +39,8 @@ module.exports.create = function createMemento( opts ){
       memento.phases = assessment.phases;
       assessment.phases = _.pluck( assessment.phases, '_id' );
     } )
-    .then( function(){
-      return comparisons.completedCount( {
+    .then( function getNumOfCompletedComparisons(){
+      return comparisonsService.completedCount( {
         assessment : opts.assessment,
         assessor   : opts.assessor
       } );
@@ -52,8 +51,22 @@ module.exports.create = function createMemento( opts ){
         comparisonsNum : memento.assessment.comparisonsNum
       };
     } )
-    .then( function(){
-      return representations.retrievePair( { assessment : opts.assessment } );
+    .then( function createComparison(){
+      var firstPhase;
+      if( memento.phases && memento.phases.length > 0 ){
+        firstPhase = memento.phases[0];
+      }
+      return comparisonsService.create( {
+        assessor   : opts.assessor,
+        assessment : opts.assessment,
+        phase      : firstPhase
+      } );
+    } )
+    .then( function handleComparison( comparison ){
+      memento.comparison = comparison;
+    } )
+    .then( function getRepresentationPair(){
+      return representationsService.retrievePair( { assessment : opts.assessment } );
     } )
     .then( function handleRepresentations( representations ){
       //todo: when no representations found
@@ -63,33 +76,36 @@ module.exports.create = function createMemento( opts ){
       representations[1].comparedNum++;
       representations[0].save();
       representations[1].save();
-      memento.representations = toSafeJSON( representations );
+      return representations;
     } )
-    .then( function(){
-      var firstPhase;
-      if( memento.phases && memento.phases.length > 0 ){
-        firstPhase = memento.phases[0];
-      }
-      return comparisons.create( {
-        assessor   : opts.assessor,
-        assessment : opts.assessment,
-        phase      : firstPhase
-      } );
-    } )
-    .then( function handleComparison( comparison ){
-      memento.comparison = comparison;
-    } )
-    .then( function(){
-      return judgements.create( {
+    .then( function createJudgements( representations ){
+      return judgementsService.create( {
         assessor        : opts.assessor,
         assessment      : opts.assessment,
-        representations : memento.representations,
+        representations : representations,
         comparison      : memento.comparison,
         positions       : ["left", "right"]
       } );
     } )
     .then( function handleJudgements( judgements ){
-      memento.judgements = judgements;
+      memento.judgements = _.sortBy( judgements, 'position' ).map(function(judgement){
+        return judgement.toJSON();
+      });
+      return judgements;
+    } )
+    .then( function listRepresentations( judgements ){
+      var promises = _.map( memento.judgements, function( judgement ){
+        return representationsService.retrieveFull( {
+          _id : judgement.representation
+        } )
+          .then( function handleRepresentation( representation ){
+            judgement._representation = representation.toSafeJSON();
+          } );
+      } );
+
+      return Promise.all( promises );
+    } )
+    .then( function(){
       return memento;
     } );
 };
@@ -104,22 +120,20 @@ module.exports.listActives = function listActives( opts ){
   debug( '#listActives' );
   var mementos = [];
 
-  return (function retrieveComparisons(){
-    return comparisons.listActive( { assessor : opts.assessor } )
-      .then( function handleComparisons( comparisons ){
-        _.each( comparisons, function( comparison ){
-          var memento = {};
-          memento.assessor = comparison.assessor;
-          memento.assessment = comparison.assessment;
-          memento.comparison = comparison;
-          comparison.assessment = comparison.assessment._id;
-          mementos.push( memento );
-        } );
+  return comparisonsService.listActive( { assessor : opts.assessor } )
+    .then( function handleComparisons( comparisons ){
+      _.each( comparisons, function( comparison ){
+        var memento = {};
+        memento.assessor = comparison.assessor;
+        memento.assessment = comparison.assessment;
+        memento.comparison = comparison;
+        comparison.assessment = comparison.assessment._id;
+        mementos.push( memento );
       } );
-  })()
-    .then( function(){
+    } )
+    .then( function getNumOfCompletedComparisons(){
       var promises = _.map( mementos, function( memento ){
-        return comparisons.completedCount( {
+        return comparisonsService.completedCount( {
           assessment : memento.assessment._id,
           assessor   : opts.assessor
         } ).then( function( completedNum ){
@@ -133,7 +147,7 @@ module.exports.listActives = function listActives( opts ){
     } )
     .then( function listPhases(){
       var promises = _.map( mementos, function( memento ){
-        return phases.list( memento.assessment.phases )
+        return phasesService.list( memento.assessment.phases )
           .then( function handlePhases( phases ){
             memento.phases = phases;
           } );
@@ -142,7 +156,7 @@ module.exports.listActives = function listActives( opts ){
     } )
     .then( function listJudgements(){
       var promises = _.map( mementos, function( memento ){
-        return judgements.list( {
+        return judgementsService.list( {
           comparison : memento.comparison
         } ).then( function handleJudgements( judgements ){
           memento.judgements = judgements;
@@ -151,18 +165,25 @@ module.exports.listActives = function listActives( opts ){
       return Promise.all( promises );
     } )
     .then( function listRepresentations(){
-      var promises = _.map( mementos, function( memento ){
-        var ids = _.pluck( memento.judgements, "representation" );
-        return representations.list( ids )
-          .then( function handleRepresentations( representations ){
-            memento.representations = representations;
+      var judgements = _.reduce( mementos, function( memo,
+                                                     memento ){
+        return memo.concat( memento.judgements );
+      }, [] );
+
+      var promises = _.map( judgements, function( judgement ){
+        return representationsService.retrieveFull( {
+          _id : judgement.representation
+        } )
+          .then( function handleRepresentation( representation ){
+            judgement.representation = representation.toSafeJSON();
           } );
       } );
+
       return Promise.all( promises );
     } )
     .then( function listSeqs(){
       var promises = _.map( mementos, function( memento ){
-        return seqs.list( {
+        return seqsService.list( {
           comparison : memento.comparison
         } ).then( function handleSeqs( seqs ){
           if( seqs && seqs.length > 0 ){
