f259149a3d0392209a358b7c37e1fe8e9c27f090
Camille Reynders <camille.reynders@gmail.com>
2015-01-06 12:28:39 +0100

    Update to express 3.3.8 specific methods

--- routes/index.js
+++ routes/index.js
@@ -73,7 +73,7 @@ exports = module.exports = function( app ){
   app.all( '/api*',
     api.middleware.requireUser );
 
-  app.del( '/api/me/session*', api.sessions.destroy );
+  app.delete( '/api/me/session*', api.sessions.destroy );
   app.all( '/api/me/session*', api.middleware.onlyAllow( 'GET, POST, DELETE' ) );
 
   app.get( '/api/me/account', api.users.retrieve );
