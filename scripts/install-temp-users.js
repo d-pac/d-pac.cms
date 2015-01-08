73c3d9484cdc9fa70e49cf7d779b62da9415eefc
Camille Reynders <camille.reynders@gmail.com>
2015-01-08 15:27:32 +0100

    Add script for creating temp users

---
+++ scripts/install-temp-users.js
@@ -0,0 +1,15 @@
+'use strict';
+var _ = require( 'underscore' );
+var path = require( 'path' );
+var pkg = require( '../package.json' );
+var fs = require( 'fs' );
+
+var source = 'add-temp-users.js';
+var script = _.template( '<%= version %>-add-temp-users-<%= now %>.js' );
+
+var src = path.resolve( path.join( __dirname, source ) );
+var dest = path.resolve( path.join( __dirname, "../updates", script( {
+  version : pkg.version,
+  now     : Date.now()
+} ) ) );
+fs.createReadStream( src ).pipe( fs.createWriteStream( dest ) );
