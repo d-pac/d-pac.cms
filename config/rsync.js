fa036e69776fb8b523af68fc5d8c15b27d754ac1
Camille Reynders <camille.reynders@gmail.com>
2015-01-20 11:59:23 +0100

    Drop devdeps and other irrelevant files from deployment

--- config/rsync.js
+++ config/rsync.js
@@ -1,25 +1,37 @@
 "use strict";
 
-module.exports = {
-  options : {
-    host      : [ "<%= env.REMOTE_USERNAME %>", "@", "<%= env.REMOTE_HOST %>" ].join( "" ),
-    args      : [ "--verbose" ],
-    recursive : true
-  },
-  app     : {
+var _ = require( "underscore" );
+
+module.exports = function( grunt,
+                           options ){
+  var devDeps = _.keys( options.pkg.devDependencies ).map( function( dep ){
+    return "node_modules/" + dep;
+  } );
+
+  return {
     options : {
-      src                : "./", // trailing slash REQUIRED [!]
-      dest               : "<%= env.REMOTE_DEST %>",
-      exclude            : [ "*-mocks.js", "public/assessors", "public/uploads", "logs", ".git*", ".DS_store" ],
-      dryRun             : false,
-      syncDestIgnoreExcl : true
+      host      : [ "<%= env.REMOTE_USERNAME %>", "@", "<%= env.REMOTE_HOST %>" ].join( "" ),
+      args      : [ "--verbose" ],
+      recursive : true
+    },
+    app     : {
+      options : {
+        src                : "./", // trailing slash REQUIRED [!]
+        dest               : "<%= env.REMOTE_DEST %>",
+        exclude            : [
+          "*-mocks.js", "public/assessors", "public/uploads", "logs", ".git*", ".DS_Store", "Gruntfile.js", "config",
+          "apidoc.json", ".editorconfig", ".grunt", ".idea", ".jscsrc", ".jshintrc", "templates"
+        ].concat( devDeps ),
+        dryRun             : false,
+        syncDestIgnoreExcl : true
+      }
+    },
+    uploads : {
+      options : {
+        src     : "./public/uploads/",
+        dest    : "<%= env.REMOTE_DEST %>/public/uploads",
+        exclude : [ ".DS_store" ]
+      }
     }
-  },
-  uploads : {
-    options : {
-      src     : "./public/uploads/",
-      dest    : "<%= env.REMOTE_DEST %>/public/uploads",
-      exclude : [ ".DS_store" ]
-    }
-  }
+  };
 };
