688ced4d6fb5acc975406ac93df979401967c71b
Camille Reynders <camille.reynders@gmail.com>
2015-05-13 14:49:20 +0200

    Disallow benchmarks to have `null` abilities

--- app/models/representations.js
+++ app/models/representations.js
@@ -12,28 +12,28 @@ var assessmentsService = require( "../services/assessments" );
 var documentsService = require( "../services/documents" );
 
 var Representation = new keystone.List( "Representation", {
-  track : true
+  track: true
 } );
 Representation.defaultColumns = "name, comparedNum";
 
 Representation.schema.methods.compareWith = function( other ){
-    this.compared.push( other._id );
-    other.compared.push( this._id );
+  this.compared.push( other._id );
+  other.compared.push( this._id );
 };
 
 require( './helpers/setupList' )( Representation )
   .add( {
-    name : {
-      type     : String,
-      default  : "Representation name",
-      noedit   : true,
-      watch    : "assessment document",
-      value    : function( callback ){
+    name: {
+      type: String,
+      default: "Representation name",
+      noedit: true,
+      watch: "assessment document",
+      value: function( callback ){
         if( this.assessment && this.document ){
           P.join( assessmentsService.retrieve( {
-            _id : this.assessment
+            _id: this.assessment
           } ), documentsService.retrieve( {
-            _id : this.document
+            _id: this.document
           } ), function( assessment,
                          document ){
             callback( null, assessmentsService.getName( assessment ) + " - " + documentsService.getName( document ) );
@@ -44,66 +44,74 @@ require( './helpers/setupList' )( Representation )
           callback( null, "Empty representation" );
         }
       },
-      required : false,
-      note     : "is automatically generated"
+      required: false,
+      note: "is automatically generated"
     },
 
-    assessment : {
-      type     : Types.Relationship,
-      ref      : "Assessment",
-      initial  : true,
-      required : true, // R02
-      many     : false, // R02
-      index    : true
+    assessment: {
+      type: Types.Relationship,
+      ref: "Assessment",
+      initial: true,
+      required: true, // R02
+      many: false, // R02
+      index: true
     },
 
-    document : {
-      type     : Types.Relationship,
-      ref      : "Document",
-      initial  : true,
-      required : true,
-      many     : false,
-      index    : true
+    document: {
+      type: Types.Relationship,
+      ref: "Document",
+      initial: true,
+      required: true,
+      many: false,
+      index: true
     },
 
-    compared : {
-      type   : Types.Relationship,
-      ref    : "Representation",
-      many   : true,
-      noedit : true
+    compared: {
+      type: Types.Relationship,
+      ref: "Representation",
+      many: true,
+      noedit: true
     },
 
-    ability : {
-      value : {
-        type    : Types.Number,
-        default : null //yes, we _really_ do want `null` here, since this is a two-state field, either with or without a value
+    ability: {
+      value: {
+        type: Types.Number,
+        default: null //yes, we _really_ do want `null` here, since this is a two-state field, either with or without a value
       },
-      se    : {
-        type    : Types.Number,
-        default : null //yes, we _really_ do want `null` here, since this is a two-state field, either with or without a value
+      se: {
+        type: Types.Number,
+        default: null //yes, we _really_ do want `null` here, since this is a two-state field, either with or without a value
       }
     },
 
-    rankType : {
-      type    : Types.Select,
-      options : constants.representationTypes.list.toString(),
-      default : constants.TO_RANK,
-      index   : true
+    rankType: {
+      type: Types.Select,
+      options: constants.representationTypes.list.toString(),
+      default: constants.TO_RANK,
+      index: true
     },
 
-    closeTo : {
-      label : "Witin range of benchmark",
-      type  : Types.Relationship,
-      ref   : "Representation",
-      many  : false
+    closeTo: {
+      label: "Witin range of benchmark",
+      type: Types.Relationship,
+      ref: "Representation",
+      many: false
     }
 
   } )
-  .retain("track")
+  .validate( {
+    "ability.value": [function(){
+      return !(this.rankType === 'benchmark' && this.ability.value === null);
+    }, "Representations of `rankType` 'benchmark' aren't allowed to have ability values of `null`"],
+    "ability.se": [function(){
+      return !(this.rankType === 'benchmark' && this.ability.se === null);
+    }, "Representations of `rankType` 'benchmark' aren't allowed to have ability SE's of `null`"]
+  } )
+  .retain( "track" )
   .relate( {
-    path    : "comparisons",
-    ref     : "Comparison",
-    refPath : "representations",
-    label   : "Comparisons"
+    path: "comparisons",
+    ref: "Comparison",
+    refPath: "representations",
+    label: "Comparisons"
   } )
   .register();
