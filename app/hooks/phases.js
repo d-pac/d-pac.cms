348c7a4f797241d1517a94f194eefa1a417c1e5a
Camille Reynders <camille.reynders@gmail.com>
2015-08-27 12:50:36 +0200

    Update phases on startup

--- app/hooks/phases.js
+++ app/hooks/phases.js
@@ -1,5 +1,7 @@
 var keystone = require( 'keystone' );
 var async = require( 'async' );
+var _ = require( 'lodash' );
+
 var constants = require( "../models/helpers/constants" );
 var Phase = keystone.list( 'Phase' );
 var Comparison = keystone.list( 'Comparison' );
@@ -8,21 +10,33 @@ function createPhase( phase,
                       done ){
   Phase.model.find( { slug: phase.slug } ).exec( function( err,
                                                            existing ){
+    var doc, action;
     if( existing && existing.length ){
-      console.log( 'Phase already exists: "' + phase.label + '"' );
-      done();
-    } else {
-      var doc = new Phase.model( phase );
-      doc.save( function( err ){
-        if( err ){
-          console.error( "Error adding phase '" + phase.label + "' to the database:" );
-          console.error( err );
-        } else {
-          console.log( "Added phase '" + phase.label + "' to the database." );
-        }
-        done();
+      doc = existing[ 0 ];
+      _.each( phase, function( val,
+                               key ){
+        doc[ key ] = val;
       } );
+      action = {
+        a: 'updating',
+        b: 'Updated'
+      };
+    } else {
+      doc = new Phase.model( phase );
+      action = {
+        a: 'adding',
+        b: 'Added'
+      };
     }
+    doc.save( function( err ){
+      if( err ){
+        console.error( "Error " + action.a + " phase '" + phase.label + "' in the database:" );
+        console.error( err );
+      } else {
+        console.log( action.b + " phase '" + phase.label + "' in the database." );
+      }
+      done();
+    } );
   } );
 }
 
