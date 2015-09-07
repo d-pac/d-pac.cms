5ffdc22a95432d22a13f4d9a2ede842a8bb94e5e
Camille Reynders <camille.reynders@gmail.com>
2015-09-07 10:19:25 +0200

    Refactor reports service

--- app/services/reports.js
+++ app/services/reports.js
@@ -9,6 +9,7 @@ var Representation = keystone.list( "Representation" );
 var assessmentsService = require( './assessments' );
 var documentsService = require( './documents' );
 var timelogsService = require( './timelogs' );
+var phasesService = require( './phases' );
 
 var LEFT_EMPTY = "empty";
 var UNDEFINED = "N/A";
@@ -32,93 +33,116 @@ module.exports.listRepresentationsForAssessmentIds = function listRepresentation
   } );
 };
 
-module.exports.listComparisonsForAssessmentIds = function listComparisonsForAssessmentIds( assessmentIds ){
-  return P.promisifyAll( Comparison.model
-      .find()
-      .where( "assessment" ).in( assessmentIds )
-      .populate( "assessor" )
-      .populate( 'representations.a' )
-      .populate( 'representations.b' )
-      .populate( 'data.selection' )
-  ).execAsync();
-};
+function getComparisonsList( assessmentIds ){
+  var p = Comparison.model
+    .find()
+    .populate( "assessor" )
+    .populate( 'representations.a' )
+    .populate( 'representations.b' )
+    .populate( 'data.selection' );
+  if( assessmentIds ){
+    assessmentIds = (_.isString( assessmentIds ))
+      ? [ assessmentIds ]
+      : assessmentIds;
+    p = p.where( "assessment" ).in( assessmentIds );
+  }
+  return P.promisify( p.exec, p )();
+}
 
 function getDocument( map,
                       representationModel ){
   return _.get( map, [ _.get( representationModel, 'document', '' ).toString(), 'name' ], UNDEFINED );
 }
 
-module.exports.listComparisons = function listComparisons( assessmentIds ){
-  var documentsById = {};
-  var assessmentsById = {};
-  var comparisonsList = [];
-  var timelogsByComparison = {};
-  return documentsService.list()
+function createMap( memo,
+                    item ){
+  var id = item.id.toString();
+  memo[ id ] = item;
+  return memo;
+}
+
+function getPhasesMap(){
+  return phasesService
+    .list()
+    .reduce( createMap, {} );
+}
+
+function getDocumentsMap(){
+  return documentsService
+    .list()
+    .reduce( createMap, {} );
+}
+
+function getAssessmentsMap( assessmentIds ){
+  var opts = { state: { $ne: null } };
+  var p = (assessmentIds)
+    ? assessmentsService.listById( assessmentIds, opts )
+    : assessmentsService.list( opts );
+  return p.reduce( createMap, {} );
+}
+
+function getTimelogsMap( comparisonIds,
+                         phasesMap ){
+  return timelogsService
+    .listForComparisonIds( null, comparisonIds )
     .reduce( function( memo,
-                       document ){
-      memo[ document.id.toString() ] = document;
+                       timelog ){
+      var comparisonId = timelog.comparison.toString();
+      _.set( memo, comparisonId + "." + timelog.phase.slug, timelog.duration );
       return memo;
-    }, documentsById )
-    .then( function(){
-      var opts = { state: { $ne: null } };
-      var p = (assessmentIds)
-        ? assessmentsService.listById( assessmentIds, opts )
-        : assessmentsService.list( opts );
-      return p.reduce( function( memo,
-                                 assessment ){
-        memo[ assessment.id.toString() ] = assessment;
-        return memo;
-      }, assessmentsById );
-    } )
-    .then( function(){
-      return module.exports.listComparisonsForAssessmentIds( _.keys( assessmentsById ) );
-    } ).then( function( comparisons ){
-      return comparisonsList = comparisons;
-    } ).then( function( comparisons ){
-      return timelogsService
-        .listForComparisonIds( null, _.pluck( comparisons, "_id" ) )
-        .reduce( function( memo,
-                           timelog ){
-          var comparisonId = timelog.comparison.toString();
-          var phase = timelog.phase.slug;
-          if( !memo[ comparisonId ] ){
-            memo[ comparisonId ] = {};
-          }
-          if( !memo[ comparisonId ][ phase ] ){
-            memo[ comparisonId ][ phase ] = 0;
-          }
-          memo[ comparisonId ][ phase ] = timelog.duration;
-          return memo;
-        }, timelogsByComparison );
-    } ).then( function(){
-      return _.map( comparisonsList, function( comparisonModel ){
-        var comparisonId = comparisonModel.id.toString();
-        return {
-          comparison: comparisonModel._rid,
-          assessment: _.get( assessmentsById, [ _.get( comparisonModel, 'assessment', '' ).toString(), 'name' ] ),
-          assessor: _.get( comparisonModel, 'assessor.email', UNDEFINED ),
-          "representation A": getDocument( documentsById, _.get( comparisonModel, 'representations.a' ) ),
-          "representation B": getDocument( documentsById, _.get( comparisonModel, 'representations.b' ) ),
-          "selected representation": getDocument( documentsById, _.get( comparisonModel, 'data.selection' ) ),
-          "completed": (comparisonModel.completed)
-            ? TRUE
-            : FALSE,
-          "comparative feedback": _.get( comparisonModel, 'data.comparative', '' ).replace( /(?:\r\n|\r|\n)/g, "\u21A9" ).replace( /"/g, "'" ),
-          "selection SEQ": _.get( comparisonModel, 'data.seq-selection', NIL ),
-          "comparative SEQ": _.get( comparisonModel, 'data.seq-comparative', NIL ),
-          "selection duration": _.get( timelogsByComparison, [ comparisonId, 'selection' ], UNDEFINED ),
-          "selection SEQ duration": _.get( timelogsByComparison, [ comparisonId, 'seq-selection' ], UNDEFINED ),
-          "comparative feedback duration": _.get( timelogsByComparison, [ comparisonId, 'comparative' ], UNDEFINED ),
-          "comparative feedback SEQ duration": _.get( timelogsByComparison, [
-            comparisonId, 'seq-comparative'
-          ], UNDEFINED ),
-          "total": _.reduce( timelogsByComparison[ comparisonId ], function( memo,
-                                                                             value ){
-            return memo + value;
-          }, 0 )
-        };
-      } );
+    }, {} );
+}
+
+function createComparisonsReportData( phasesMap,
+                                      documentsMap,
+                                      assessmentsMap,
+                                      comparisonsList ){
+  return function( timelogsMap ){
+    return _.map( comparisonsList, function( comparisonModel ){
+      var comparisonId = comparisonModel.id.toString();
+      return {
+        comparison: comparisonModel._rid,
+        assessment: _.get( assessmentsMap, [ _.get( comparisonModel, 'assessment', '' ).toString(), 'name' ] ),
+        assessor: _.get( comparisonModel, 'assessor.email', UNDEFINED ),
+        "representation A": getDocument( documentsMap, _.get( comparisonModel, 'representations.a' ) ),
+        "representation B": getDocument( documentsMap, _.get( comparisonModel, 'representations.b' ) ),
+        "selected representation": getDocument( documentsMap, _.get( comparisonModel, 'data.selection' ) ),
+        "completed": (comparisonModel.completed)
+          ? TRUE
+          : FALSE,
+        "comparative feedback": _.get( comparisonModel, 'data.comparative', '' ).replace( /(?:\r\n|\r|\n)/g, "\u21A9" ).replace( /"/g, "'" ),
+        "selection SEQ": _.get( comparisonModel, 'data.seq-selection', NIL ),
+        "comparative SEQ": _.get( comparisonModel, 'data.seq-comparative', NIL ),
+        "selection duration": _.get( timelogsMap, [ comparisonId, 'selection' ], UNDEFINED ),
+        "selection SEQ duration": _.get( timelogsMap, [ comparisonId, 'seq-selection' ], UNDEFINED ),
+        "comparative feedback duration": _.get( timelogsMap, [ comparisonId, 'comparative' ], UNDEFINED ),
+        "comparative feedback SEQ duration": _.get( timelogsMap, [
+          comparisonId, 'seq-comparative'
+        ], UNDEFINED ),
+        "total": _.reduce( timelogsMap[ comparisonId ], function( memo,
+                                                                  value ){
+          return memo + value;
+        }, 0 )
+      };
     } );
+  };
+}
+
+module.exports.listComparisons = function listComparisons( assessmentIds ){
+  return P.join(
+    getPhasesMap(),
+    getDocumentsMap(),
+    getAssessmentsMap( assessmentIds ),
+    getComparisonsList( assessmentIds ),
+
+    function( phasesMap,
+              documentsMap,
+              assessmentsMap,
+              comparisonsList ){
+      return getTimelogsMap( _.pluck( comparisonsList, "_id" ) )
+        .then( createComparisonsReportData( phasesMap, documentsMap, assessmentsMap, comparisonsList ) );
+    }
+  );
 };
 
 module.exports.listRepresentations = function listRepresentations( assessmentIds ){
