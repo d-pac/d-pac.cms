bed9a5161d44eade99d672aee45f63b44ab4f0a6
Camille Reynders <camille.reynders@gmail.com>
2015-09-15 15:57:29 +0200

    Update moment-range API calls

--- app/models/timelogs.js
+++ app/models/timelogs.js
@@ -2,19 +2,20 @@
 
 var keystone = require( "keystone" );
 var Types = keystone.Field.Types;
-var moment = require( "moment-range" );
+var moment = require( "moment" );
+require( 'moment-range' );
 
 var format = "DD/MM/YYYY HH:mm:ss";
 var Timelog = new keystone.List( "Timelog", {
-  map         : {
-    name : "id"
+  map: {
+    name: "id"
   },
-  track       : true,
-  defaultSort : "comparison"
+  track: true,
+  defaultSort: "comparison"
 } );
 
 Timelog.api = {
-  editable : [ "comparison", "phase", "begin", "end" ]
+  editable: [ "comparison", "phase", "begin", "end" ]
 };
 
 Timelog.defaultColumns = "name, comparison, phase, begin, end, duration";
@@ -22,40 +23,41 @@ Timelog.defaultColumns = "name, comparison, phase, begin, end, duration";
 var list = require( './helpers/setupList' )( Timelog )
   .add( {
 
-    phase : {
-      type     : Types.Relationship,
-      ref      : "Phase",
-      required : true,
-      initial  : true
+    phase: {
+      type: Types.Relationship,
+      ref: "Phase",
+      required: true,
+      initial: true
     },
 
-    comparison : {
-      type    : Types.Relationship,
-      ref     : "Comparison",
-      require : true,
-      initial : true
+    comparison: {
+      type: Types.Relationship,
+      ref: "Comparison",
+      require: true,
+      initial: true
     },
 
-    begin : {
-      type     : Types.Datetime,
-      required : true,
-      initial  : true,
-      format   : format
+    begin: {
+      type: Types.Datetime,
+      required: true,
+      initial: true,
+      format: format
     },
 
-    end : {
-      type     : Types.Datetime,
-      required : false,
-      initial  : true,
-      format   : format
+    end: {
+      type: Types.Datetime,
+      required: false,
+      initial: true,
+      format: format
     }
 
   } )
-  .retain('track')
+  .retain( 'track' )
   .virtualize( {
-    duration : function(){
+    duration: function(){
       if( this.end ){
-        return moment.range( this.begin, this.end ).diff( "s" );
+        var d = moment.range( this.begin, this.end );
+        return d.diff( "s" );
       }
 
       return 0;
