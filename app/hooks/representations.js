52a734abd91941ec5803827dca3fda0e06dcfa54
Camille Reynders <camille.reynders@gmail.com>
2015-11-23 11:26:16 +0100

    Automatically remove relevant comparisons when representation is removed

---
+++ app/hooks/representations.js
@@ -0,0 +1,25 @@
+'use strict';
+var _ = require( 'lodash' );
+var P = require( 'bluebird' );
+var keystone = require( 'keystone' );
+
+var representationsService = require( '../services/representations' );
+var comparisonsService = require( '../services/comparisons' );
+
+function representationRemovedHandler( done ){
+  var representation = this;
+  comparisonsService.listForRepresentation( representation )
+    .each( function( comparison ){
+      return P.promisify( comparison.remove, comparison )();
+    } )
+    .then(function(){
+      done();
+    })
+    .catch(function(err){
+      done(err);
+    });
+}
+
+module.exports.init = function(){
+  keystone.list( 'Representation' ).schema.pre( 'remove', representationRemovedHandler );
+};
