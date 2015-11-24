5a28208bcd60caf9133fcce5454c0c3405910754
Camille Reynders <camille.reynders@gmail.com>
2015-11-24 10:47:34 +0100

    fixup! Store time stamp when a selection is made

--- app/services/timelogs.js
+++ app/services/timelogs.js
@@ -25,3 +25,24 @@ module.exports.listForComparisonIds = function listForComparisonIds( comparisonI
     .where( "comparison" ).in( comparisonIds )
     .execAsync();
 };
+
+module.exports.retrieveFinalForComparisons = function( phaseId, comparisonIds){
+  return base.list( {
+      phase: phaseId
+    } )
+    .where("comparison").in(comparisonIds)
+    .sort( "-end" )
+    .execAsync()
+    .then( function( timelogs ){
+      if( timelogs && timelogs.length ){
+        return _.chain(timelogs)
+          .groupBy("comparison")
+          .reduce(function(memo, timelogs, id){
+            memo[id] = timelogs[0];
+            return memo;
+          }, {})
+          .value();
+      }
+      return {};
+    } );
+};
---
+++ app/updates/0.6.19-selection-timestamps.js
@@ -0,0 +1,55 @@
+'use strict';
+var _ = require( 'lodash' );
+var P = require( 'bluebird' );
+var path = require( 'path' );
+
+var phasesService = require( '../services/phases' );
+var comparisonsService = require( '../services/comparisons' );
+var timelogsService = require( '../services/timelogs' );
+
+var log = _.partial( console.log, path.basename( __filename ) + ':' );
+
+exports = module.exports = function( done ){
+  P.join( phasesService.list( {
+      slug: 'selection'
+    } ),
+    comparisonsService.list( {
+      selectionMadeAt: { $eq: null }
+    } ),
+    function( phases,
+              comparisons ){
+      var selectionPhase = phases[ 0 ];
+      comparisons = _.reduce( comparisons, function( memo,
+                                                     comparison ){
+        var id = comparison.id;
+        memo.map[ id ] = comparison;
+        memo.keys.push( id );
+        return memo;
+      }, {
+        keys: [],
+        map: {}
+      } );
+      return timelogsService.retrieveFinalForComparisons( selectionPhase.id, comparisons.keys )
+        .then( function( timelogsByComparisonId ){
+          return _.reduce( timelogsByComparisonId, function( saveQueue,
+                                                             timelog,
+                                                             comparisonId ){
+            var comparison = comparisons.map[ comparisonId ];
+            comparison.selectionMadeAt = timelog.end;
+            saveQueue.push( comparison );
+            return saveQueue;
+          }, [] );
+        } )
+    } )
+    .then( function( saveQueue ){
+      //we're going to yield here, to allow keystone bootstrapping to continue
+      done();
+      return saveQueue;
+    } )
+    .each( function( doc ){
+      return P.promisify( doc.save, doc )();
+    } )
+    .then( function( comparisons ){
+      log( 'Updated', comparisons.length, "comparisons" );
+    } );
+};
