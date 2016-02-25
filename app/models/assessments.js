e2b4b005a902a6210b4f82e0fc80cea10b77fbc3
Camille Reynders <camille.reynders@gmail.com>
2016-02-25 09:30:15 +0100

    DPAC-107 - (Assessments) add assessorsNum to limits virtual

--- app/models/assessments.js
+++ app/models/assessments.js
@@ -345,12 +345,13 @@ require( './helpers/setupList' )( Assessment )
           comparisonsNum: {
             perAssessor: pA,
             perRepresentation: pR
-          }
+          },
+          assessorsNum: _.get( assessment, [ 'assessors', 'minimum' ], 0 )
         }
       },
       depends: [
         'comparisons.perAssessor', 'comparisons.perRepresentation', 'comparisons.dimension', 'cache.representationsNum',
-        'cache.assessorNum'
+        'cache.assessorNum', 'assessors.minimum'
       ]
     }
   } )
@@ -368,7 +369,7 @@ require( './helpers/setupList' )( Assessment )
       }, '"UI texts" contains invalid JSON'
     ]
   } )
-  .retain( "track" )
+  .retain( "track", "assessors", "comparisons" )
   .relate( {
     path: "representations",
     ref: "Representation",
