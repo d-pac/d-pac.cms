95b6d31e02d7354cb24f81a1c8e558aeb31a4f1b
Camille Reynders <camille.reynders@gmail.com>
2016-03-09 16:13:38 +0100

    (Updates) script for creating a default user

---
+++ app/updates/0.0.1-default-user.js
@@ -0,0 +1,15 @@
+var keystone = require('keystone');
+var User = keystone.list('User');
+
+module.exports = function (done) {
+	new User.model({
+		name: {
+			first: 'Default',
+			last: 'User'
+		},
+		email: 'default@d-pac.be',
+		password: 'changeme',
+		isAdmin: true
+	})
+	.save(done);
+};
