8cd944e1d95bcbc2e120dcbe07ebad1d5d6e4ac8
Camille Reynders <camille.reynders@gmail.com>
2016-04-05 09:24:23 +0200

    Remove related timelogs when comparison removed

---
+++ app/hooks/timelogs.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const keystone = require( 'keystone' );
+const handleHook = require( './helpers/handleHook' );
+const timelogsService = require( '../services/timelogs' );
+
+function removeTimelogsForComparison( comparison ){
+  return timelogsService.list( {
+      comparison: comparison.id
+    } )
+    .mapSeries( ( timelog )=>timelog.remove() );
+}
+
+module.exports.init = function(){
+  keystone.list( 'Comparison' ).schema.post( 'remove', handleHook( removeTimelogsForComparison ) );
+};
