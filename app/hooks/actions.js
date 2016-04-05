1a8d4217419d0e4f642229a49400faebc4c94d58
Camille Reynders <camille.reynders@gmail.com>
2016-04-05 11:55:58 +0200

    Rewrite assessment deletion hooks and actions

--- app/hooks/actions.js
+++ app/hooks/actions.js
@@ -1,287 +1,77 @@
 'use strict';
 
-var _ = require( 'lodash' );
-var keystone = require( 'keystone' );
-var P = require( 'bluebird' );
-var path = require( 'path' );
-var exec = P.promisify( require( 'child_process' ).exec );
-var moment = require( 'moment' );
-var url = require( 'url' );
+const P = require( 'bluebird' );
+const keystone = require( 'keystone' );
 
-var comparisonsService = require( '../services/comparisons' );
-var representationsService = require( '../services/representations' );
-var assessmentsService = require( '../services/assessments' );
-var usersService = require( '../services/users' );
-var constants = require( '../models/helpers/constants' );
+const assessmentsService = require( '../services/assessments' );
+const service = require( '../services/actions' );
+const handleHook = require( './helpers/handleHook' );
 
-var Representation = keystone.list( 'Representation' );
-var Timelog = keystone.list( 'Timelog' );
-
-var dumpCommandTpl = _.template( 'mongodump ' +
-  '--host {{host}} ' +
-  '--db {{db}} ' +
-  "--out '{{out}}' " +
-  '--collection {{collection}} ' +
-  "--query '{{query}}'", {
-  interpolate: /{{([\s\S]+?)}}/g
-} );
-
-function deleteAssessmentAssociates( assessmentId ){
-  return comparisonsService.list( {
-      assessment: assessmentId.toString()
-    } )
-    .each( function( comparison ){
-      return comparison.remove();
-    } )
-    .then( function( comparisonsList ){
-      return _.map( comparisonsList, "id" );
-    } )
-    .then( function( comparisonIds ){
-      return Timelog.model.remove( {
-        comparison: {
-          $in: comparisonIds
-        }
-      } );
-    } );
-}
-
-function resetAssessment( assessmentId ){
-  return deleteAssessmentAssociates( assessmentId )
-    .then( function(){
-      return representationsService.list( {
-        assessment: assessmentId
-      } );
-    } )
-    .each( function( representation ){
-      representation.compared = [];
-      return representation.save();
-    } )
-    .then( function(){
-      return assessmentsService.retrieve( {
-        _id: assessmentId.toString()
-      } );
-    } )
-    .then( function( assessment ){
-      assessment.reset();
-      return assessment.save();
-    } );
-}
-
-function clearAssessment( assessmentId ){
-  return deleteAssessmentAssociates( assessmentId )
-    .then( function(){
-      return representationsService.list( {
-        assessment: assessmentId
-      } );
-    } )
-    .each( function( representation ){
-      return representation.remove();
-    } )
-    .then( function(){
-      return assessmentsService.retrieve( {
-        _id: assessmentId.toString()
-      } );
-    } )
-    .then( function( assessment ){
-      assessment.reset();
-      return assessment.save();
-    } );
-
-}
-
-function removeAssessmentFromUser( assessmentId,
-                                   user,
-                                   fieldName ){
-  var index = _.get( user, [ "assessments", fieldName ], [] ).indexOf( assessmentId );
-  if( index >= 0 ){
-    user.assessments[ fieldName ].splice( index, 1 );
-    return true;
-  }
-  return false;
-}
-
-function deleteAssessment( assessmentId ){
-  return deleteAssessmentAssociates( assessmentId )
-    .then( function(){
-      return Representation.model.remove( {
-        assessment: assessmentId
-      } );
-    } )
-    .then( function(){
-      return usersService.list();
-    } )
-    .reduce( function( memo,
-                       user ){
-      var needsSaving = _.reduce( constants.roles.list, function( dirty,
-                                                                  role ){
-        return removeAssessmentFromUser( assessmentId, user, role.value ) || dirty;
-      }, false );
-      if( needsSaving ){
-        memo.push( user );
-      }
-      return memo;
-    }, [] )
-    .each( function( user ){
-      return user.save();
-    } )
-    .then( function(){
-      return assessmentsService.remove( {
-        _id: assessmentId.toString()
-      } );
-    } );
-}
-
-function archiveAssessment( assessmentId ){
-  var uriObj = url.parse( keystone.get( 'mongo' ), false, true );
-  var baseArgs = {
-    host: uriObj.host,
-    db: uriObj.pathname.substring( 1 )
-  };
-  var assessment;
-  var result = '';
-  return assessmentsService.retrieve( { _id: assessmentId.toString() } )
-    .then( function( doc ){
-      assessment = doc;
-      baseArgs.out = path.resolve( path.join( constants.directories.archive, doc.name
-        + '-' + moment().format( 'YYYYMMDD-HHmmss' ) ) );
-    } )
-    .then( function(){
-      return comparisonsService.list( {
-          assessment: assessmentId.toString()
-        } )
-        .then( function( comparisonsList ){
-          return _.map( comparisonsList, "id" );
-        } );
-    } )
-    .map( function( id ){
-      return {
-        $oid: id
-      };
-    } )
-    .then( function( comparisonIds ){
-      var command = dumpCommandTpl( _.defaults( {}, baseArgs, {
-        collection: 'timelogs',
-        query: JSON.stringify( {
-          comparison: { $in: comparisonIds }
-        } )
-      } ) );
-
-      result += command + '<br/>';
-      return exec( command ).spread( function( stdout,
-                                               stderr ){
-        result += stdout;
-      } );
-    } )
-    .then( function(){
-      var command = dumpCommandTpl( _.defaults( {}, baseArgs, {
-        collection: 'assessments',
-        query: JSON.stringify( {
-          _id: { $oid: assessmentId.toString() }
-        } )
-      } ) );
-
-      result += command + '<br/>';
-      return exec( command ).spread( function( stdout,
-                                               stderr ){
-        result += stdout;
-      } );
-    } )
-    .then( function(){
-      var command = dumpCommandTpl( _.defaults( {}, baseArgs, {
-        collection: 'representations',
-        query: JSON.stringify( {
-          assessment: { $oid: assessmentId.toString() }
-        } )
-      } ) );
-
-      result += command + '<br/>';
-      return exec( command ).spread( function( stdout,
-                                               stderr ){
-        result += stdout;
-      } );
-    } )
-    .then( function(){
-      var command = dumpCommandTpl( _.defaults( {}, baseArgs, {
-        collection: 'comparisons',
-        query: JSON.stringify( {
-          assessment: { $oid: assessmentId.toString() }
-        } )
-      } ) );
-
-      result += command + '<br/>';
-      return exec( command ).spread( function( stdout,
-                                               stderr ){
-        result += stdout;
-      } );
-    } )
-    .then( function(){
-      return deleteAssessment( assessmentId );
-    } )
-    .then( function(){
-      return {
-        out: result,
-        assessment: assessment
-      };
-    } );
-}
-
-function actionCreatedHandler( next ){
-  var action = this;
+function executeAction( action ){
 
   function failureHandler( err ){
     action.line = "FAILED";
     action.success = false;
     action.log = err;
-    next();
+    return P.resolve();
   }
+
+  const p = assessmentsService.retrieve( { _id: action.assessment } );
 
   switch( action.actionType ){
     case "reset":
-      resetAssessment( action.assessment )
+      return p.then( function( assessment ){
+          return service.resetAssessment( assessment )
+        } )
         .then( function( assessment ){
           action.line = "Assessment: " + assessment.name;
           action.log = "Successfully reset";
           action.success = true;
-          next();
         } )
         .catch( failureHandler );
-      break;
+
     case "clear":
-      clearAssessment( action.assessment )
+      return p.then( function( assessment ){
+          return service.clearAssessment( assessment )
+        } )
         .then( function( assessment ){
           action.line = "Assessment: " + assessment.name;
           action.log = "Successfully cleared";
           action.success = true;
-          next();
         } )
         .catch( failureHandler );
-      break;
+
     case "delete":
-      deleteAssessment( action.assessment )
+      return p.then( function( assessment ){
+          return service.deleteAssessment( assessment )
+        } )
         .then( function( assessment ){
           action.line = "Assessment: " + assessment.name;
           action.log = "Successfully deleted";
           action.success = true;
-          next();
         } )
         .catch( failureHandler );
-      break;
-    case "archive":
-      archiveAssessment( action.assessment )
+
+    case "archive" :
+      return p.then( function( assessment ){
+          return service.archiveAssessment( assessment )
+        } )
         .then( function( result ){
           action.line = "Assessment: " + result.assessment.name;
           action.log = result.out;
           action.success = true;
-          next();
         } )
         .catch( failureHandler );
-      break;
+
     default:
-      next( new Error( "Unhandled action: " + action.actionType ) );
+      return P.reject( new Error( "Unhandled action: " + action.actionType ) );
   }
 }
 
 module.exports.init = function(){
-  keystone.list( 'Action' ).schema.pre( 'save', actionCreatedHandler );
+  keystone.list( 'Action' ).schema.pre( 'save', handleHook( executeAction ) );
+  keystone.list( 'Assessment' ).schema.pre( 'remove', handleHook( ( assessment )=>{
+    return service.prepAssessmentForDeletion( assessment );
+  } ) );
 };
 
---
+++ app/services/actions.js
@@ -0,0 +1,175 @@
+'use strict';
+
+var _ = require( 'lodash' );
+var keystone = require( 'keystone' );
+var P = require( 'bluebird' );
+var path = require( 'path' );
+var exec = P.promisify( require( 'child_process' ).exec );
+var moment = require( 'moment' );
+var url = require( 'url' );
+
+var comparisonsService = require( '../services/comparisons' );
+var representationsService = require( '../services/representations' );
+var assessmentsService = require( '../services/assessments' );
+var usersService = require( '../services/users' );
+var constants = require( '../models/helpers/constants' );
+
+var Representation = keystone.list( 'Representation' );
+var Timelog = keystone.list( 'Timelog' );
+
+var dumpCommandTpl = _.template( 'mongodump ' +
+  '--host {{host}} ' +
+  '--db {{db}} ' +
+  "--out '{{out}}' " +
+  '--collection {{collection}} ' +
+  "--query '{{query}}'", {
+  interpolate: /{{([\s\S]+?)}}/g
+} );
+
+module.exports.deleteAssessmentAssociates = function deleteAssessmentAssociates( assessment ){
+  return comparisonsService.list( {
+      assessment: assessment.id
+    } )
+    .mapSeries( function( comparison ){
+      return comparison.remove();
+    } );
+};
+
+module.exports.resetAssessment = function resetAssessment( assessment ){
+  return module.exports.deleteAssessmentAssociates( assessment )
+    .then( function(){
+      return representationsService.list( {
+        assessment: assessment.id
+      } );
+    } )
+    .mapSeries( function( representation ){
+      representation.compared = [];
+      return representation.save();
+    } )
+    .then( function(){
+      assessment.reset();
+      return assessment.save();
+    } );
+};
+
+module.exports.clearAssessment = function clearAssessment( assessment ){
+  return module.exports.deleteAssessmentAssociates( assessment.id )
+    .then( function(){
+      return representationsService.list( {
+        assessment: assessment.id
+      } );
+    } )
+    .mapSeries( function( representation ){
+      return representation.remove();
+    } )
+    .then( function(){
+      assessment.reset();
+      return assessment.save();
+    } );
+
+};
+
+module.exports.prepAssessmentForDeletion= function prepAssessmentForDeletion(assessment){
+  return module.exports.deleteAssessmentAssociates( assessment )
+    .then( function(){
+      return Representation.model.remove( {
+        assessment: assessment.id
+      } );
+    } )
+};
+
+module.exports.deleteAssessment = function deleteAssessment( assessment ){
+  return module.exports.prepAssessmentForDeletion(assessment)
+    .then( function(){
+      return assessmentsService.remove( {
+        _id: assessment.id
+      } );
+    } );
+};
+
+module.exports.archiveAssessment = function archiveAssessment( assessment ){
+  var uriObj = url.parse( keystone.get( 'mongo' ), false, true );
+  var baseArgs = {
+    host: uriObj.host,
+    db: uriObj.pathname.substring( 1 ),
+    out: path.resolve( path.join( constants.directories.archive, assessment.name
+      + '-' + moment().format( 'YYYYMMDD-HHmmss' ) ) )
+  };
+  var result = '';
+  return comparisonsService.list( {
+      assessment: assessment.id
+    } )
+    .then( function( comparisonsList ){
+      return _.map( comparisonsList, "id" );
+    } )
+    .map( function( id ){
+      return {
+        $oid: id
+      };
+    } )
+    .then( function( comparisonIds ){
+      var command = dumpCommandTpl( _.defaults( {}, baseArgs, {
+        collection: 'timelogs',
+        query: JSON.stringify( {
+          comparison: { $in: comparisonIds }
+        } )
+      } ) );
+
+      result += command + '<br/>';
+      return exec( command ).spread( function( stdout,
+                                               stderr ){
+        result += stdout;
+      } );
+    } )
+    .then( function(){
+      var command = dumpCommandTpl( _.defaults( {}, baseArgs, {
+        collection: 'assessments',
+        query: JSON.stringify( {
+          _id: { $oid: assessment.id }
+        } )
+      } ) );
+
+      result += command + '<br/>';
+      return exec( command ).spread( function( stdout,
+                                               stderr ){
+        result += stdout;
+      } );
+    } )
+    .then( function(){
+      var command = dumpCommandTpl( _.defaults( {}, baseArgs, {
+        collection: 'representations',
+        query: JSON.stringify( {
+          assessment: { $oid: assessment.id }
+        } )
+      } ) );
+
+      result += command + '<br/>';
+      return exec( command ).spread( function( stdout,
+                                               stderr ){
+        result += stdout;
+      } );
+    } )
+    .then( function(){
+      var command = dumpCommandTpl( _.defaults( {}, baseArgs, {
+        collection: 'comparisons',
+        query: JSON.stringify( {
+          assessment: { $oid: assessment.id }
+        } )
+      } ) );
+
+      result += command + '<br/>';
+      return exec( command ).spread( function( stdout,
+                                               stderr ){
+        result += stdout;
+      } );
+    } )
+    .then( function(){
+      return module.exports.deleteAssessment( assessment );
+    } )
+    .then( function(){
+      return {
+        out: result,
+        assessment: assessment
+      };
+    } );
+};
