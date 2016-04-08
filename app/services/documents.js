54e12ca6ede5a417f2dcc293b27c41230b0bde01
Camille Reynders <camille.reynders@gmail.com>
2016-04-08 13:38:36 +0200

    (Documents) fix service for documents w/o file

--- app/services/documents.js
+++ app/services/documents.js
@@ -13,16 +13,22 @@ const base = new Service( collection );
 module.exports = base.mixin();
 
 module.exports.create = function( opts ){
-  debug('#create');
-  const source = opts.file.path;
-  opts.file.path = path.join( constants.directories.documents, opts.file.filename );
-  return fs.renameAsync( source, opts.file.path )
-    .catch( ( err )=>P.reject( err ) )
-    .then( ()=>base.create( opts ) );
+  debug( '#create' );
+  let p;
+  if( opts.file ){
+    const source = opts.file.path;
+    opts.file.path = path.join( constants.directories.documents, opts.file.filename );
+    p = fs.renameAsync( source, opts.file.path )
+      .catch( ( err )=>P.reject( err ) );
+
+  } else {
+    p = P.resolve();
+  }
+  return p.then( ()=>base.create( opts ) );
 };
 
-module.exports.update = function(opts){
-  debug('#update');
+module.exports.update = function( opts ){
+  debug( '#update' );
   const source = opts.file.path;
   opts.file.path = path.join( constants.directories.documents, opts.file.filename );
   opts.title = '';
