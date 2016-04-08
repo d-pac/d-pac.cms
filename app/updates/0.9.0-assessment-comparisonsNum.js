f6f6b4355a5490c792c978b5458d0e1a8dd120cc
Camille Reynders <camille.reynders@gmail.com>
2016-04-08 13:01:15 +0200

    Fix assessment comparisonsNum update script

--- app/updates/0.9.0-assessment-comparisonsNum.js
+++ app/updates/0.9.0-assessment-comparisonsNum.js
@@ -20,26 +20,27 @@ module.exports = function( done ){
         _id: doc._id,
       };
       let dim = 'representation';
-      if( _.get(doc, ['comparisonsNum','perRepresentation'], 0) !== 0 ){
+      if( _.get( doc, [ 'comparisonsNum', 'perRepresentation' ], 0 ) !== 0 ){
         update[ "comparisons.perRepresentation" ] = doc.comparisonsNum.perRepresentation;
       }
-      const pA = _.get(doc, ['comparisonsNum','perAssessor'], []);
-      if( pA.length !== 0 && pA[0] !== 0){
-        update[ "comparisons.perAssessor" ] = pA[0];
+      const pA = _.get( doc, [ 'comparisonsNum', 'perAssessor' ], [] );
+      if( pA.length !== 0 ){
+        update[ "comparisons.perAssessor" ] = _.reduce( pA, ( memo,
+                                                              value )=>memo + value, 0 );
         dim = 'assessor';
       }
       if( doc.algorithm === 'benchmarked-comparative-selection' ){
         update[ "assessorsNum.minimum" ] = 4;
       }
-      if( typeof _.get(doc, ['comparisons','dimension'], undefined) === 'undefined' ){
+      if( typeof _.get( doc, [ 'comparisons', 'dimension' ], undefined ) === 'undefined' ){
         update[ "comparisons.dimension" ] = dim;
       }
 
       return P.props( {
-          comparisonsNum: comparisonsService.count( { assessment: update._id } ),
-          representationsNum: representationsService.countToRanks( { assessment: update._id } ),
-          assessorsNum: usersService.countInAssessment( 'assessor', update._id )
-        } )
+        comparisonsNum: comparisonsService.count( { assessment: update._id } ),
+        representationsNum: representationsService.countToRanks( { assessment: update._id } ),
+        assessorsNum: usersService.countInAssessment( 'assessor', update._id )
+      } )
         .then( ( values )=>{
           update[ "cache.representationsNum" ] = values.representationsNum;
           update[ "cache.comparisonsNum" ] = values.comparisonsNum;
@@ -51,8 +52,8 @@ module.exports = function( done ){
     .then( function( updates ){
 
       return P.mapSeries( updates, ( update )=>{
-          return assessmentsService.collection.model.update( { _id: update._id }, update );
-        } )
+        return assessmentsService.collection.model.update( { _id: update._id }, update );
+      } )
         .catch( ( err )=>done( err ) );
     } )
     .then( function( results ){
