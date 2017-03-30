af9f10b5d36cf5c209bdf240ac69e2d0737fc0a7
Camille Reynders <camille.reynders@gmail.com>
2017-03-30 12:05:45 +0200

    Add update script to migrate assessments

--- app/services/stats.js
+++ app/services/stats.js
@@ -1,159 +1,163 @@
 'use strict';
 
-const _ = require( 'lodash' );
-const keystone = require( 'keystone' );
-const async = require( 'async' );
-const debug = require( "debug" )( "dpac:services.stats" );
-const estimate = require( 'estimating-rasch-model' );
-const P = require( 'bluebird' );
-const diff = require( 'deep-diff' ).diff;
-const usersService = require( './users' );
-const comparisonsService = require( './comparisons' );
-const representationsService = require( './representations' );
-const timelogsService = require( './timelogs' );
-const fns = require( 'd-pac.functions' );
+const _ = require('lodash');
+const keystone = require('keystone');
+const async = require('async');
+const debug = require("debug")("dpac:services.stats");
+const estimate = require('estimating-rasch-model');
+const P = require('bluebird');
+const diff = require('deep-diff').diff;
+const usersService = require('./users');
+const comparisonsService = require('./comparisons');
+const representationsService = require('./representations');
+const timelogsService = require('./timelogs');
+const fns = require('d-pac.functions');
 
-const getAbility = _.partialRight( _.get, [ 'ability', 'value' ] );
-const getSE = function( item ){
-  const se = _.get( item, [ 'ability', 'se' ], 0 );
-  if( se > 3 ){
+const getAbility = _.partialRight(_.get, ['ability', 'value']);
+const getSE = function (item) {
+  const se = _.get(item, ['ability', 'se'], 0);
+  if (se > 3) {
     return 3;
   }
   return se;
 };
-const getReliability = fns.pm.reliabilityFunctor( getAbility, getSE );
+const getReliability = fns.pm.reliabilityFunctor(getAbility, getSE);
 
 module.exports = {
-  estimate: function( representations,
-                      comparisons ){
-    debug( "#estimate" );
+  estimate: function (representations,
+                      comparisons) {
+    debug("#estimate");
     let representationDocs, representationObjs;
     let comparisonDocs, comparisonObjs;
-    if( _.isArray( representations ) ){
+    if (_.isArray(representations)) {
       representationDocs = representations;
-      representationObjs = JSON.parse( JSON.stringify( representationDocs ) );
+      representationObjs = JSON.parse(JSON.stringify(representationDocs));
     } else {
       representationDocs = representations.documents;
       representationObjs = representations.objects;
     }
 
-    if( _.isArray( comparisons ) ){
+    if (_.isArray(comparisons)) {
       comparisonDocs = comparisons;
-      comparisonObjs = JSON.parse( JSON.stringify( comparisonDocs ) );
+      comparisonObjs = JSON.parse(JSON.stringify(comparisonDocs));
     } else {
       comparisonDocs = comparisons.documents;
       comparisonObjs = comparisons.objects;
     }
 
     let succeed;
-    const promise = new P( function( resolve/*,
-                                   reject*/ ){
+    const promise = new P(function (resolve/*,
+                                     reject*/) {
       succeed = resolve;
-    } );
+    });
 
-    setTimeout( function(){
-      try{
-        estimate.estimateCJ( comparisonObjs, representationObjs );
-      } catch( err ) {
-        console.log( err, err.stack );
+    setTimeout(function () {
+      try {
+        estimate.estimateCJ(comparisonObjs, representationObjs);
+      } catch (err) {
+        console.log(err, err.stack);
         //return fail( err );
       }
-      const toRanks = _.filter( representationObjs, function( representation ){
+      const toRanks = _.filter(representationObjs, function (representation) {
         return representation.rankType === "to rank";
-      } );
+      });
 
       const saveQueue = [];
 
-      _.forEach( toRanks, function( representationObj ){
-        const doc = _.find( representationDocs, function( representationDoc ){
+      _.forEach(toRanks, function (representationObj) {
+        const doc = _.find(representationDocs, function (representationDoc) {
           return representationDoc.id.toString() === representationObj._id;
-        } );
-        const diffObj = diff( JSON.parse( JSON.stringify( doc.ability ) ), representationObj.ability );
-        if( diffObj ){
-          console.log( "Differences for", representationObj.name, ":", diffObj );
-          _.forEach( representationObj.ability, function( value,
-                                                          key ){
-            doc.ability[ key ] = representationObj.ability[ key ];
-          } );
-          saveQueue.push( doc );
+        });
+        const diffObj = diff(JSON.parse(JSON.stringify(doc.ability)), representationObj.ability);
+        if (diffObj) {
+          console.log("Differences for", representationObj.name, ":", diffObj);
+          _.forEach(representationObj.ability, function (value,
+                                                         key) {
+            const v = representationObj.ability[key];
+            if (!isNaN(v)) {
+              doc.ability[key] = v;
+            }
+          });
+          saveQueue.push(doc);
         }
-      } );
+      });
 
-      async.eachSeries( saveQueue, function( representation,
-                                             next ){
-        representation.save( next );
-      }, function( err ){
-        if( err ){
-          console.log( err );
+      async.eachSeries(saveQueue, function (representation,
+                                            next) {
+        representation.save(next);
+      }, function (err) {
+        if (err) {
+          console.log(err);
           //return fail( err );
         }
         //console.log( "Updated representations:", saveQueue.length );
-        succeed( saveQueue );
-      } );
-    }, 500 );
+        succeed(saveQueue);
+      });
+    }, 500);
 
     return promise;
   },
 
-  estimateForAssessment: function( assessmentId ){
-    debug( "#estimateForAssessment" );
-    const getComparisons = keystone.list( "Comparison" ).model.find( { assessment: assessmentId } );
-    const getRepresentations = keystone.list( "Representation" ).model.find( { assessment: assessmentId } );
+  estimateForAssessment: function (assessmentId) {
+    debug("#estimateForAssessment");
+    const getComparisons = keystone.list("Comparison").model.find({assessment: assessmentId});
+    const getRepresentations = keystone.list("Representation").model.find({assessment: assessmentId});
 
     const self = this;
-    return P.join( getComparisons.exec(), getRepresentations.exec(), function( comparisonDocs,
-                                                                               representationDocs ){
-      return self.estimate( representationDocs, comparisonDocs );
-    } );
+    return P.join(getComparisons.exec(), getRepresentations.exec(), function (comparisonDocs,
+                                                                              representationDocs) {
+      return self.estimate(representationDocs, comparisonDocs);
+    });
   },
 
-  statsForAssessment: function( assessment ){
+  statsForAssessment: function (assessment) {
     const assessmentId = assessment.id;
-    return P.props( {
-        assessors: usersService.listForAssessments( 'assessor', [ assessmentId ] ),
-        comparisons: comparisonsService.listForAssessments( {}, [ assessmentId ] ),
-        toRankRepresentations: representationsService.list( {
-          assessment: assessmentId,
-          rankType: "to rank"
-        } )
-      } )
-      .then( function( docs ){
-        docs.timelogs = timelogsService.listForComparisonIds( _.map( docs.comparisons, '_id' ) );
-        return P.props( docs );
-      } )
-      .then( function( docs ){
+    return P.props({
+      assessors: usersService.listForAssessments('assessor', [assessmentId]),
+      comparisons: comparisonsService.listForAssessments({}, [assessmentId]),
+      toRankRepresentations: representationsService.list({
+        assessment: assessmentId,
+        rankType: "to rank"
+      })
+    })
+      .then(function (docs) {
+        docs.timelogs = timelogsService.listForComparisonIds(_.map(docs.comparisons, '_id'));
+        return P.props(docs);
+      })
+      .then(function (docs) {
+        const r = getReliability(docs.toRankRepresentations);
         const totals = {
-          reliability: getReliability( docs.toRankRepresentations ),
+          reliability: (!isNaN(r)) ? r : null,
           assessorsNum: docs.assessors.length,
           comparisonsNum: docs.comparisons.length,
           representationsNum: docs.toRankRepresentations.length,
-          duration: _.reduce( docs.timelogs, function( memo,
-                                                       timelog ){
+          duration: _.reduce(docs.timelogs, function (memo,
+                                                      timelog) {
             return memo + timelog.duration;
-          }, 0 )
+          }, 0)
         };
-        const byRepresentation = _.reduce( docs.comparisons, function( memo,
-                                                                     comparison ){
+        const byRepresentation = _.reduce(docs.comparisons, function (memo,
+                                                                      comparison) {
           const aId = comparison.representations.a.toString();
           const bId = comparison.representations.b.toString();
-          _.set( memo, [ aId, 'comparisonsNum' ], _.get( memo, [ aId, 'comparisonsNum' ], 0 ) + 1 );
-          _.set( memo, [ bId, 'comparisonsNum' ], _.get( memo, [ bId, 'comparisonsNum' ], 0 ) + 1 );
+          _.set(memo, [aId, 'comparisonsNum'], _.get(memo, [aId, 'comparisonsNum'], 0) + 1);
+          _.set(memo, [bId, 'comparisonsNum'], _.get(memo, [bId, 'comparisonsNum'], 0) + 1);
           return memo;
-        }, {} );
+        }, {});
         const averages = {};
-        if(totals.representationsNum > 0){
-          averages.comparisonsPerRepresentation= (totals.comparisonsNum / totals.representationsNum) * 2;
-          averages.durationPerRepresentation= totals.duration / totals.representationsNum;
+        if (totals.representationsNum > 0) {
+          averages.comparisonsPerRepresentation = (totals.comparisonsNum / totals.representationsNum) * 2;
+          averages.durationPerRepresentation = totals.duration / totals.representationsNum;
         }
-        if(totals.assessorsNum>0){
-          averages.comparisonsPerAssessor= totals.comparisonsNum / totals.assessorsNum;
-          averages.durationPerAssessor= totals.duration / totals.assessorsNum;
+        if (totals.assessorsNum > 0) {
+          averages.comparisonsPerAssessor = totals.comparisonsNum / totals.assessorsNum;
+          averages.durationPerAssessor = totals.duration / totals.assessorsNum;
         }
         return {
           totals: totals,
           averages: averages,
           byRepresentation: byRepresentation
         };
-      } );
+      });
   },
 };
