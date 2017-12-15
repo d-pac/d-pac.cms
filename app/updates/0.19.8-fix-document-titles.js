4421832146dec7824fb09f06a39e0ebd2058d5a1
Camille Reynders <camille.reynders@gmail.com>
2017-12-15 09:24:35 +0100

    Hotfix update-script fix-doc-titles

--- app/updates/0.19.8-fix-document-titles.js
+++ app/updates/0.19.8-fix-document-titles.js
@@ -4,14 +4,18 @@ const documentsService = require('../services/documents');
 const log = _.partial(console.log, require('path').basename(__filename) + ':');
 
 module.exports = function (done) {
+  const errored  =[];
   documentsService.list({})
     .mapSeries((doc) => {
       doc.title = '';
       doc.representation=false;
-      return doc.save();
+      return doc.save()
+        .catch((err)=>{
+          errored.push(doc.id);
+        });
     })
     .then((docs) => {
-      log(`Updated ${docs.length} documents`);
+      log(`Processed ${docs.length} documents, with ${errored.length} errors`);
       return null;
     })
     .asCallback(done);
